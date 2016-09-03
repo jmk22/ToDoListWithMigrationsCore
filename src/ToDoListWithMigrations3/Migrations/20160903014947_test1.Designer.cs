0df8d65c2f1dec962097ff1a0cdf8753f59172c9
jill kuchman <pillanoid@gmail.com>
2016-09-02 18:50:48 -0700

    Initial commit with migrations working

---
+++ src/ToDoListWithMigrations3/Migrations/20160903014947_test1.Designer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using ToDoListWithMigrations3.Models;
+
+namespace ToDoListWithMigrations3.Migrations
+{
+    [DbContext(typeof(ToDoWM3DbContext))]
+    [Migration("20160903014947_test1")]
+    partial class test1
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.0.0-rc2-20901")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("ToDoListWithMigrations3.Models.Category", b =>
+                {
+                    b.Property<int>("CategoryId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Name");
+
+                    b.HasKey("CategoryId");
+
+                    b.ToTable("Categories");
+                });
+
+            modelBuilder.Entity("ToDoListWithMigrations3.Models.Item", b =>
+                {
+                    b.Property<int>("ItemId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("CategoryId");
+
+                    b.Property<string>("Description");
+
+                    b.HasKey("ItemId");
+
+                    b.HasIndex("CategoryId");
+
+                    b.ToTable("Items");
+                });
+
+            modelBuilder.Entity("ToDoListWithMigrations3.Models.Item", b =>
+                {
+                    b.HasOne("ToDoListWithMigrations3.Models.Category")
+                        .WithMany()
+                        .HasForeignKey("CategoryId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+        }
+    }
+}
